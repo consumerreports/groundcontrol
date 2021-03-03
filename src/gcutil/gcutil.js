94ce4f1e6c0cbed38effd1cb0405ed957a699f83
Noah Levenson <noahlevenson@gmail.com>
2021-03-03 17:07:18 -0500

    docs complete for gcutil

--- src/gcutil/gcutil.js
+++ src/gcutil/gcutil.js
@@ -1,18 +1,34 @@
+/**
+* Utility functions
+* @namespace gcutil
+*/
+
 "use strict";
 
 const crypto = require("crypto");
 const util = require("util");
 
-// Calculate the SHA256 hash of some data
-// TODO: this makes the sketchy assumption that the host's OpenSSL implementation has SHA256 available
-// If weird stuff is happening, try 'openssl list -digest-algorithms'
+/**
+* Compute the SHA256 hash of some data
+* @memberof gcutil
+* @param {string} data - the data to hash
+* @returns {string}
+*/
 function sha256(data) {
+    // TODO: this assumes that the host's OpenSSL implementation includes SHA256
+    // if weird/bad things are happening, check 'openssl list -digest-algorithms'
     const hash = crypto.createHash("SHA256");
     hash.update(data);
     return hash.digest("hex");
 }
 
-// Deep inspect an object, useful for debugging
+/**
+* Deep inspect an object
+* @memberof gcutil
+* @param {Object} obj - the object to inspect
+* @param {number} [depth=20] - recursion depth
+* @returns {string}
+*/
 function inspect(obj, depth = 20) {
     return util.inspect(obj, {showHidden: false, depth: depth, colors: true, compact: 40, breakLength: 200});
 }
